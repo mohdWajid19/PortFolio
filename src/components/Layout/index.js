d5732d1fea481c2db69db2c4faa9bb511feb9aec
mohdWajid <mohdwajeed1919@gmail.com>
2022-06-30 15:23:12 +0530

    added text workUnderProgress

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,11 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import "animate.css"
 
 const Layout = () => {
     return <>
     <Sidebar />
+    <h2 className="work animate__animated animate__bounce">WORK UNDER PROGRESS</h2>
     </>
 
 }
