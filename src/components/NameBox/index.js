b4b3b7242a098f1c2fa9b57b43222626086b3f85
mohdWajid <mohdwajeed1919@gmail.com>
2022-09-05 12:39:43 +0530

    marquee tag deprecated so replaced it

--- src/components/NameBox/index.js
+++ src/components/NameBox/index.js
@@ -3,8 +3,9 @@ import './index.scss';
 export default function NameBox(props) {
     return <div className="namebox">
         <div className="box">
-            <a href="https://drive.google.com/file/d/1N77FaGDnsdAQcMwCP5IMJB4GJ7FDoXmC/view?usp=sharing"><marquee scrollamount="10">{props.name}'s  portfolio</marquee></a>
+            {/* <a href=""><marquee scrollamount="10">{props.name}'s  portfolio</marquee></a> */}
             {/* <span scrollamount="10">{props.name}'s  portfolio</span> */}
+            <a href="https://drive.google.com/file/d/1N77FaGDnsdAQcMwCP5IMJB4GJ7FDoXmC/view?usp=sharing"><span scrollamount="10">{props.name}'s Resume</span></a>
         </div>
     </div>
 }
