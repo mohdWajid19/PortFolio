ebe0c2fbd60340af5d148fd7048d1241745ef69c
MohdWajid19 <mohdwajeed1919@gmail.com>
2023-04-15 13:04:35 +0530

    added new code

--- src/components/DevDetails/index.js
+++ src/components/DevDetails/index.js
@@ -1,14 +1,17 @@
 import "./index.scss";
+import mypic from "../../assets/images/mypic.jpg"
 
 export default function DevDetails() {
     return (<div className="dev-details">
         <div className="top-container">
-            <div className="image">Pic</div>
+            <div className="image">
+                <img class="dev-image"src={mypic} alt="dev-image" />
+            </div>
             <div className="details">
-                <div className="profile-detail one">Developer</div>
-                <div className="profile-detail two">Intern...</div>
+                <div className="profile-detail one">Developer.<div className="caret"></div></div>
+                <div className="profile-detail two">Intern...<div className="caret"></div></div>
             </div>
         </div>
-        <div className="bottom-container profile-detail">Learner..</div>
-    </div>);
+        <div className="bottom-container profile-detail">Learner..<div className="caret"></div></div>
+        </div>);
 }
